2780bbb72417cb0a2cc0c8c416e56aaf6318341d
daniel <danieldzotsi24@gmail.com>
2024-10-29 07:50:40 +0000

    doned

--- sneakfreak/sneakfreak/urls.py
+++ sneakfreak/sneakfreak/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 from core.views import *
 urlpatterns = [
-    path('', index, name="index"),
-    path('contact/', contact, name="contact"),
+    path('', include("core.urls")),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
+    path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
+    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
